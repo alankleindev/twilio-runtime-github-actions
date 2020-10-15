608c1e832a793d45af15d9c7f9a10c55116a3329
alankleindev <57042275+alankleindev@users.noreply.github.com>
2020-10-15 06:59:46 -0400

    Update hello-world.js
    Make change from Winston to Winston Klein

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
     const result = {
-        message : "Hello Winston!"
+        message : "Hello Winston Klein!"
     }
 
     callback(null, result);
